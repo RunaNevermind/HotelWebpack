23b9bda2dd9083148958b12cafe9019bcad74f73
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-04-22 19:29:02 +0500

    Верстка дропдауна

---
+++ src/assets/pages/form-element/dropdown-quest/dropdown-quest.js
@@ -0,0 +1,16 @@
+let incAdult = document.getElementById('increment-adult');
+let decAdult = document.getElementById('decrement-adult');
+let adult = document.getElementById('adult');
+
+let incChild = document.getElementById('increment-child');
+let decChild = document.getElementById('decrement-child');
+
+let incInfant = document.getElementById('increment-Infant');
+let decInfant = document.getElementById('decrement-Infant');
+
+let allQuest = 0;
+
+incAdult.addEventListener('click', function(){
+    allQuest += 0
+    int.innerHTML = allQuest
+})
