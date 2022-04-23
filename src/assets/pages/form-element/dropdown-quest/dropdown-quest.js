b0a2f16beb1151c0534ab3db35030db85ec1fb94
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-04-23 20:59:52 +0500

    сделала dropdown

--- src/assets/pages/form-element/dropdown-quest/dropdown-quest.js
+++ src/assets/pages/form-element/dropdown-quest/dropdown-quest.js
@@ -1,16 +1,91 @@
 let incAdult = document.getElementById('increment-adult');
 let decAdult = document.getElementById('decrement-adult');
-let adult = document.getElementById('adult');
+let adult = 0;
 
 let incChild = document.getElementById('increment-child');
 let decChild = document.getElementById('decrement-child');
+let child=0;
 
-let incInfant = document.getElementById('increment-Infant');
-let decInfant = document.getElementById('decrement-Infant');
+let incInfant = document.getElementById('increment-infant');
+let decInfant = document.getElementById('decrement-infant');
+let infant = 0;
+
+let clear = document.getElementById('button-clear');
+let accept = document.getElementById('button-accept')
 
 let allQuest = 0;
+var div = document.getElementById('button-clear');
 
 incAdult.addEventListener('click', function(){
-    allQuest += 0
-    int.innerHTML = allQuest
+    adult++
+    allQuest++
+    document.querySelector('#allQuest').innerHTML = allQuest + ' гостя'
+    document.querySelector('#adult').innerHTML = adult
+    div.style.visibility='visible';
+})
+
+decAdult.addEventListener('click', function(){
+    if(adult > 0){
+        adult--
+        allQuest--
+        document.querySelector('#allQuest').innerHTML = allQuest + ' гостя'
+        document.querySelector('#adult').innerHTML = adult
+    }
+    else
+        return 0
+})
+
+incChild.addEventListener('click', function(){
+    allQuest++
+    child++
+    document.querySelector('#allQuest').innerHTML = allQuest + ' гостя'
+    document.querySelector('#child').innerHTML = child
+})
+
+decChild.addEventListener('click', function(){
+    if(child > 0){
+        child--
+        allQuest--
+        document.querySelector('#allQuest').innerHTML = allQuest + ' гостя'
+        document.querySelector('#child').innerHTML = child
+    }
+    else
+        return 0
+})
+
+incInfant.addEventListener('click', function(){
+    infant++
+    allQuest++
+    document.querySelector('#allQuest').innerHTML = allQuest + ' гостя'
+    document.querySelector('#infant').innerHTML = infant
+})
+
+decInfant.addEventListener('click', function(){
+    if(infant > 0){
+        infant--
+        allQuest--
+        document.querySelector('#allQuest').innerHTML = allQuest  + ' гостя'
+        document.querySelector('#infant').innerHTML = infant
+    }
+    else
+        return 0
+})
+
+clear.addEventListener('click', function(){
+    adult= 0;
+    document.querySelector('#adult').innerHTML = adult
+    child=0;
+    document.querySelector('#child').innerHTML = child
+    infant=0;
+    document.querySelector('#infant').innerHTML = infant
+    allQuest=0;
+    document.querySelector('#allQuest').innerHTML = allQuest
+    if(allQuest==0){
+        document.querySelector('#allQuest').innerHTML = 'Сколько гостей';
+    }    
+})
+
+accept.addEventListener('click', function(){
+    allQuest = adult + child + infant;
+    document.querySelector('#allQuest').innerHTML = allQuest + ' гостя';
 })
