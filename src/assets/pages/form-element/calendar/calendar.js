a2a107557ce1f491a7991ad1315e495eabfce05c
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-05-14 21:15:32 +0500

    сделан календарь

--- src/assets/pages/form-element/calendar/calendar.js
+++ src/assets/pages/form-element/calendar/calendar.js
@@ -3,20 +3,30 @@ import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 import './calendar.scss';
 
-let accept = {
+var accept = {
     content: 'Принять',
     className: 'accept-button-classname',
     onClick: () => {
     }
 }
+var start= document.getElementById('air-datepicker');
+var end= document.getElementById('end');
+
 new AirDatepicker('#air-datepicker', {
     range:true,
     clearButton: true,
-    inline: true,
     toggleSelected: false,
+
     navTitles: {
         days: '<strong>yyyy</strong> <i>MMMM</i>',
         months: 'Select month of <strong>yyyy</strong>'    
     },
-    buttons: ['clear', accept]
+    buttons: ['clear', accept],
+    minDate: new Date(),
+    disableNavWhenOutOfRange:false,
+    onSelect:function onselect(date){
+        if(date.formattedDate.length>1){
+            start.value = date.formattedDate.shift(' ');
+            end.value = date.formattedDate.pop(' ');
+    }   }
 });
