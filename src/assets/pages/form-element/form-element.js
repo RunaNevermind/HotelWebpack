635bdb94a61454bb564bf4b48e6f6e6cbe6d5790
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-06-11 20:07:33 +0500

    radio-button

--- src/assets/pages/form-element/form-element.js
+++ src/assets/pages/form-element/form-element.js
@@ -2,4 +2,5 @@ import './calendar/calendar.js';
 import './form-element.scss';
 import './dropdown-quest/dropdown-quest.js'
 import './input/input.js'
+import './checkbox/checkbox.js'
 import './radio-button/radio-button.js'
