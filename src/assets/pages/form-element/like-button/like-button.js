49cef72217adc0cad8659051f7949650f2cdd000
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-06-26 20:24:47 +0500

    rate button

--- src/assets/pages/form-element/like-button/like-button.js
+++ src/assets/pages/form-element/like-button/like-button.js
@@ -1,11 +1,20 @@
 import './like-button.scss';
 
-let increment = document.querySelector('.like-button');
-let likeCounter = document.querySelector('.like-button__wrap__like').value;
+let increment = document.querySelector('.like-button__wrap');
+let likeCounter = document.querySelector('.like-button__wrap__like__text').innerHTML;
+let button = document.querySelector('.like-button__wrap__button');
 
+document.querySelector('.like-button__wrap__like__text').innerHTML=likeCounter;
+let like=likeCounter;
 increment.addEventListener('click', function(){
-    console.log(likeCounter)    
-    likeCounter++;
-    document.querySelector('.like-button__wrap__like').innerHTML=likeCounter;
-})
-
+    if(like==likeCounter){
+        like++;         
+        button.checked=true;
+        return document.querySelector('.like-button__wrap__like__text').innerHTML=like;
+    }
+    else{
+        like--;
+        button.checked=false;
+        return document.querySelector('.like-button__wrap__like__text').innerHTML=like;
+    }
+})    
--- src/assets/pages/form-element/rate-button/rate-button.js
+++ src/assets/pages/form-element/rate-button/rate-button.js
@@ -1,1 +1,16 @@
-import'./rate-button.scss'
+import'./rate-button.scss';
+
+const star1 = document.querySelector('#star1');
+const star2 = document.querySelector('#star2');
+const star3 = document.querySelector('#star3');
+const star4 = document.querySelector('#star4');
+const star5 = document.querySelector('#star5');
+
+var labelRate = document.querySelector('.rate-button__label');
+var inputRate = document.querySelector('.rate-button__input');
+var star = document.querySelector('.rate-button')
+var starButton = document.querySelector('.rate-button__star')
+
+star.addEventListener('click', function(){
+    console.log(starButton.value)
+})
